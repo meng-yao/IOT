700116460eeff1da40499ba3d18663baaecabc9f
meng-yao <smt8922@gmail.com>
2016-12-27 11:59:20 +0000

    thermal

--- wukong-darjeeling/wukong/gateway/udpwkpf/testD6T.py
+++ wukong-darjeeling/wukong/gateway/udpwkpf/testD6T.py
@@ -16,22 +16,36 @@ while(True):
     #print len(result)
     #print test
     print 'Enviroment Temp:',(result[1]<<8)|result[0]
-    print 'P0:',(result[3]<<8)|result[2]
-    print 'P1:',(result[5]<<8)|result[4]
-    print 'P2:',(result[7]<<8)|result[6]
-    print 'P3:',(result[9]<<8)|result[8]
-    print 'P4:',(result[11]<<8)|result[10]
-    print 'P5:',(result[13]<<8)|result[12]
-    print 'P6:',(result[15]<<8)|result[14]
-    print 'P7:',(result[17]<<8)|result[16]
-    print 'P8:',(result[19]<<8)|result[18]
-    print 'P9:',(result[21]<<8)|result[20]
-    print 'P10:',(result[23]<<8)|result[22]
-    print 'P11:',(result[25]<<8)|result[24]
-    print 'P12:',(result[27]<<8)|result[26]
-    print 'P13:',(result[29]<<8)|result[28]
-    print 'P14:',(result[31]<<8)|result[30]
-    print 'P15:',(result[33]<<8)|result[32]
+    temp = [
+            (result[3]<<8) | result[2],
+            (result[5]<<8) | result[4],
+            (result[7]<<8) | result[6],
+            (result[9]<<8) | result[8],
+            (result[11]<<8) | result[10],
+            (result[13]<<8) | result[12],
+            (result[15]<<8) | result[14],
+            (result[17]<<8) | result[16],
+            (result[19]<<8) | result[18],
+            (result[21]<<8) | result[20],
+            (result[23]<<8) | result[22],
+            (result[25]<<8) | result[24],
+            (result[27]<<8) | result[26],
+            (result[29]<<8) | result[28],
+            (result[31]<<8) | result[30],
+            (result[33]<<8) | result[32]]
+    print temp
+    for x in range (0, 16):
+        if temp[x] > 230:
+            temp[x] = 1
+        else:
+            temp[x] = 0
+    for y in range(0, 4):
+        print temp[y*4],temp[y*4+1],temp[y*4+2],temp[y*4+3]
+
+    #print (result[3]<<8)|result[2],' ',(result[5]<<8)|result[4],' ',(result[7]<<8)|result[6],' ',(result[9]<<8)|result[8]
+    #print (result[11]<<8)|result[10],' ',(result[13]<<8)|result[12],' ',(result[15]<<8)|result[14],' ',(result[17]<<8)|result[16]
+    #print (result[19]<<8)|result[18],' ',(result[21]<<8)|result[20],' ',(result[23]<<8)|result[22],' ',(result[25]<<8)|result[24]
+    #print (result[27]<<8)|result[26],' ',(result[29]<<8)|result[28],' ',(result[31]<<8)|result[30],' ',(result[33]<<8)|result[32]
     time.sleep(1)
 
 
